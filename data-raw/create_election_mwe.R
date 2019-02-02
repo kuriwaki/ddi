7f8c6eec7313d6561b0db1228c028cf99b16f96f
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-02-02 00:00:05 -0500

    create data

---
+++ data-raw/create_election_mwe.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+df <- readRDS("~/Dropbox/poll_error/data/output/df_joined.rds")
+
+g2016 <- df %>%
+  select(state, st, pct_djt_voters,
+         cces_pct_djt_vv, cces_pct_djtrund_vv,
+         votes_djt, tot_votes, cces_n_vv,
+         vap, vep)
+
+
+usethis::use_data(g2016, overwrite = TRUE)
