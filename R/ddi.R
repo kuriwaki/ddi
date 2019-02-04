677406c77b0d5db24820d02513fe6e4a71284874
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-02-04 17:25:00 -0500

    change input to vectors for #1. Also change order of arguments

--- R/ddi.R
+++ R/ddi.R
@@ -5,18 +5,35 @@
 #' are binary. Squaring the d.d.c. is more useful for characterizing the asymptotics of
 #' MSE.
 #'
-#' @param data dataframe
-#' @param N variable name (char) for population size. Default assumes \code{"N"}
-#' @param n variable name (char) for sample size. Default assumes \code{"n"}
-#' @param mu variable name (char) for population quantity of interest
-#' @param muhat variable name (char) for sample estimate
+#' @param mu Vector of population quantity of interest
+#' @param muhat Vector for sample estimate
+#' @param N Vector of population size
+#' @param n Vector of sample size
+#' @param cv Coeffcient of variation  of the weights, if survey weights exist.
+#'   \code{sd(w) / mean(w)}
 #'
-#' @export
+#' @return A vector of d.d.c. of the same length of the input, or a scalar if
+#'  all input variables are scalars.
 #'
 #' @examples
+#' library(tibble)
+#' library(dplyr)
+#'
 #' data(g2016)
-#' ddcs <- ddc(g2016, N = "tot_votes", n = "cces_n_vv", mu = "pct_djt_voters", muhat = "cces_pct_djt_vv")
-#' tibble(st = g2016$st, ddc = ddcs)
+#'
+#' # 1. scalar input
+#' select(g2016, cces_totdjt_vv, cces_n_vv, tot_votes, votes_djt) %>%
+#'   summarize_all(sum)
+#'
+#' ## plug those numbers in
+#' ddc(mu = 62984824/136639786, muhat = 12284/35829, N = 136639786, n = 35829)
+#'
+#' # 2. vector input using "with"
+#' with(g2016, ddc(mu = pct_djt_voters, muhat = cces_pct_djt_vv, N = tot_votes, n = cces_n_vv))
+#'
+#' # 3. vector input in tidy tibble
+#' transmute(g2016, st,
+#'  ddc = ddc(mu = pct_djt_voters, muhat = cces_pct_djt_vv, N = tot_votes, n = cces_n_vv))
 #'
 #'
 #' @references Meng, Xiao-Li. Statistical paradises and paradoxes in big data (I):
@@ -24,21 +41,23 @@
 #' Ann. Appl. Stat. 12 (2018), no. 2, 685--726. \url{http://doi.org/10.1214/18-AOAS1161SF}
 #'
 #'
-ddc <- function(data = df, N = "N", n = "n", mu,  muhat, cv = NULL) {
+#' @export
+ddc <- function(mu, muhat, N, n, cv = NULL) {
 
-
-  N <- data[[N]]
-  n <- data[[n]]
-  mu <- data[[mu]]
-  muhat <- data[[muhat]]
   if (!is.null(cv)) cv <- data[[cv]]
 
   ## parts
   one_over_sqrtN <- 1 / sqrt(N)
   diff_mu <- muhat - mu
+
+  #  ((1- f) / f) = (N - n)/n "Data Quantity"
   f <- n / N
   one_minus_f <- 1 - f
+
+  ## sigma, "Problem Difficulty"
   s2hat <- mu * (1 - mu)
+
+  # adjustment factor for SEs when there are wieghts
   if (!is.null(cv)) {
     A <- sqrt(1 + (cv^2 / one_minus_f))
     one_over_A <- 1 /A
